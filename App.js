fccb31a902c6727c708629f371b7f6184eb2e9cf
BlackNugget <23023898@myrp.edu.sg>
2024-10-25 14:21:51 +0800

    Updated Bananas

--- App.js
+++ App.js
@@ -1,20 +1,120 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Exercise 1 App.js
+// import React from 'react';
+// import { View, Text } from 'react-native';
+//
+// const Movie = ({ title, year }) => {
+//   return (
+//       <View>
+//         <Text>{title} {year}</Text>
+//       </View>
+//   );
+// };
+//
+// const AllMovies = () => {
+//   return (
+//       <View>
+//         <Text></Text>
+//         <Text></Text>
+//         <Movie title = "Doctor Sleep" year = {2019} icon_name = "paw"/>
+//       </View>
+//   )
+// }
+// export default AllMovies;
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// Exercise 2
+// import React from 'react';
+// import { View, Text } from 'react-native';
+// import Icon from "react-native-vector-icons/FontAwesome6";
+//
+//
+// const Movie = ({ title, year, icon_name }) => {
+//   return (
+//       <View>
+//         <Text>{title} {year}</Text>
+//         <Icon name = {icon_name} size={100} color = "#B23B23" />
+//       </View>
+//   );
+// };
+//
+// const AllMovies = () => {
+//   return (
+//       <View>
+//         <Text></Text>
+//         <Text></Text>
+//         <Movie title = "Doctor Sleep" year = {2019} icon_name = "paw"/>
+//       </View>
+//   )
+// }
+// export default AllMovies;
+
+// Exercise 3-4
+// import React from 'react';
+// import { View, Text, Image } from 'react-native';
+// import Icon from "react-native-vector-icons/FontAwesome6";
+//
+//
+// const Movie = ({ title, year, icon_name, poster }) => {
+//     return (
+//         <View>
+//             < Image source={poster} style = {{width:400, height:500}}/>
+//             <Text>{title} {year}</Text>
+//             <Icon name = {icon_name} size={100} color = "#B23B23" />
+//         </View>
+//     );
+// };
+//
+// const AllMovies = () => {
+//     return (
+//         <View>
+//             <Text></Text>
+//             <Text></Text>
+//             <Movie title = "Doctor Sleep" year = {2019} icon_name = "paw" poster = {require('./img/doctor-sleep.jpg')}/>
+//         </View>
+//     )
+// }
+// export default AllMovies;
+
+// Exercise 5
+// import React from 'react';
+// import { View, Text, Image } from 'react-native';
+// import Movie from './components/Movie';
+//
+// const AllMovies = () => {
+//     return (
+//         <View>
+//             <Text></Text>
+//             <Text></Text>
+//             <Movie title = "Doctor Sleep" year = {2019} icon_name = "paw"
+//                    poster = {require('./img/doctor-sleep.jpg')}/>
+//
+//             <Movie title = "Midway" year = {2020} icon_name = "person-rifle"
+//                    poster = {require('./img/midway.jpg')}/>
+//         </View>
+//     )
+// }
+// export default AllMovies;
+
+// Exercise 6
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import Movie from './components/Movie';
+import { ScrollView } from 'react-native';
+
+const AllMovies = () => {
+    return (
+        <View>
+            <ScrollView>
+            <Text></Text>
+            <Text></Text>
+            <Movie title = "Doctor Sleep" year = {2019} icon_name = "paw"
+                   poster = {require('./img/doctor-sleep.jpg')}/>
+
+            <Movie title = "Midway" year = {2020} icon_name = "person-rifle"
+                   poster = {require('./img/midway.jpg')}/>
+            </ScrollView>
+        </View>
+    )
 }
+export default AllMovies;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// Exercise 7
